275309816d4b4b0facd9d1ad7a12d08ffd76c08b
amir meyari <forsite73@gmail.com>
2019-10-13 15:44:57 +0330

    passowrdInput mixin added

--- src/modules/shared/PasswordInput.ts
+++ src/modules/shared/PasswordInput.ts
@@ -1,9 +1,12 @@
-import { Field, InputType } from "type-graphql";
+import { Field, InputType, ClassType } from "type-graphql";
 import { Length } from "class-validator";
 
-@InputType()
-export class PasswordInput {
-  @Field()
-  @Length(1, 255)
-  password: string;
-}
+export const PasswordMixin = <T extends ClassType>(BaseClass: T) => {
+  @InputType()
+  class PasswordInput extends BaseClass {
+    @Field()
+    @Length(1, 255)
+    password: string;
+  }
+  return PasswordInput;
+};
--- src/modules/user/register/RegisterInput.ts
+++ src/modules/user/register/RegisterInput.ts
@@ -1,11 +1,11 @@
 import { Field, InputType } from "type-graphql";
 import { IsEmail, Length } from "class-validator";
 import { IsEmailAlreadyExist } from "./guards/isEmailAlreadyExist";
-import { PasswordInput } from "../../shared/PasswordInput";
+import { PasswordMixin } from "../../shared/PasswordInput";
 
 @InputType()
 // export class RegisterInput implements Partial<User> {
-export class RegisterInput extends PasswordInput {
+export class RegisterInput extends PasswordMixin(class {}) {
   @Field()
   @Length(1, 255)
   name: string;
