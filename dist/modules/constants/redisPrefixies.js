483f65bb25956c87ea1bc28a119032aa7fb5d4e5
amir meyari <forsite73@gmail.com>
2019-11-01 01:36:41 +0330

    remove dist from git

--- dist/modules/constants/redisPrefixies.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.confrimationPrefix = "user-confirmation:";
-exports.forgotPasswordPrefix = "forgot-password:";
-//# sourceMappingURL=redisPrefixies.js.map
--- dist/utils/calcCompelexity.js
+++
@@ -1,1 +0,0 @@
-//# sourceMappingURL=calcCompelexity.js.map
